77e107f8fce2916ed57bd083008077004db0b2d7
YousefShata <yossef_shata_a@yahoo.com>
2024-07-05 19:25:44 -0700

    fix stylewq

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,7 +2,7 @@
 """
 element_length  module
 """
-from typing import Iterable, Sequence,List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
