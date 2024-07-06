1c75d826788543b3a84bee0a2da121c1c47aa197
YousefShata <yossef_shata_a@yahoo.com>
2024-07-05 18:53:15 -0700

    sum mix

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+ sum_list module
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    sum_list function
+    """
+
+    return sum(mxd_lst)
