4507b028040f145954030bcf749270bf2ffc9a4f
YousefShata <yossef_shata_a@yahoo.com>
2024-07-05 18:58:44 -0700

    to_kv

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+ sum_list module
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    sum_list function
+    """
+
+    return (k, v ** 2)
