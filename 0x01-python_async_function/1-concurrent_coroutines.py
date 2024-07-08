bec78b75ddb31c0e1e50beaf0af115b10a00f89e
YousefShata <yossef_shata_a@yahoo.com>
2024-07-08 15:36:59 -0700

    apply fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     use random generator fucntion N of times
     """
