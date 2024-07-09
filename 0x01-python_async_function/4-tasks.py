6eebe5d33713521bc98c16d68c32f42d03e50c67
YousefShata <yossef_shata_a@yahoo.com>
2024-07-08 17:08:01 -0700

    fixed pycodestyle

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,6 +7,7 @@ import random
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     use random generator fucntion N of times
